c64c449f637c1b2309b92ef079410684c69aa079
Parker Wilson <parkerw103@gmail.com>
2022-11-03 00:16:11 -0500

    Create Problem_1to2.m

---
+++ C9B-Tapered_Roller_Bearings-Variable_Loading/Problem_1to2.m
@@ -0,0 +1,14 @@
+%%%%% Problems 1-2 %%%%%
+% A countershaft is supported by two tapered roller bearings using an 
+% indirect mounting. The radial bearing loads are 560 lbf for the left-hand 
+% bearing and 1095 for the right-hand bearing. An axial load of 200 lbf is 
+% carried by the left bearing. The shaft rotates at 480 rev/min and is to 
+% have a desired life of 31 kh. Use an application factor of 1.4 and a 
+% combined reliability goal of 0.93, assuming distribution data from 
+% manufacturer 1 in Table 11–6. Using an initial K = 1.5, find the required 
+% radial rating for each bearing. Select the bearings from Fig. 11–15.
+
+% Problem 1
+% What are the induced loads at each of the bearings, the design life 
+% ratio, xD, and the required reliability for each bearing?
+
