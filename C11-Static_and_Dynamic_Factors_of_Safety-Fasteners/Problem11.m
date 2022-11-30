01681ab5dd2e941b2208db372c90872f80d1b02b
Parker Wilson <parkerw103@gmail.com>
2022-11-30 01:04:15 -0600

    Update Problems

--- C11-Static_and_Dynamic_Factors_of_Safety-Fasteners/Problem11.m
+++ C11-Static_and_Dynamic_Factors_of_Safety-Fasteners/Problem11.m
@@ -6,3 +6,28 @@
 %
 % Determine the bolt diameter, bolt length, and the joint constant.
 
+d = 14; % mm %%%%%% CHANGE %%%%%
+E_steel = 207; % GPa
+l = 40; % mm
+A = 0.78715; % for steel
+B = 0.62873; % for steel
+L = 55; % mm
+
+if L <= 125
+    L_t = 2*d + 6; % mm
+elseif (125 < L) && (L <= 200)
+    L_t = 2*d + 12; % mm
+else
+    L_t = 2*d + 25; % mm
+end
+
+l_d = L - L_t;
+l_t = l - l_d;
+
+A_d = pi*d^2/4; % mm^2
+A_t = 115; % from table 8-1 %%%%%% CHANGE %%%%%
+
+k_b = (A_d*A_t*E_steel) / (A_d*l_t + A_t*l_d);
+k_m = E_steel*d*A*exp(B*d/l);
+
+C = k_b/(k_b + k_m);
